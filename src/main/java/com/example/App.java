11dc81b7b1747aff8ffcb2af0242f61082d0a78d
AmrutaTechStack <dashraja920@gmail.com>
2025-01-16 10:41:14 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,8 +6,11 @@ package com.example;
  */
 public class App 
 {
+    int i =10;
+    int j = 20;
     public static void main( String[] args )
     {
+    
         System.out.println( "Hello World!" );
     }
 }
